482f63047efca402ab7059c47bcc9f9e0eb11b43
Lefteris <el.avramidis@gmail.com>
2019-09-26 21:37:15 +0100

    Added new example

---
+++ examples/example_3.cpp
@@ -0,0 +1,45 @@
+//---------------------------------------------------------------------------//
+// Copyright (c) 2019 Eleftherios Avramidis <el.avramidis@gmail.com>
+//
+// Distributed under The MIT License (MIT)
+// See accompanying file LICENSE.txt
+//---------------------------------------------------------------------------//
+
+#include <iostream>
+#include "Timers.hpp"
+#include <thread>
+
+int main() {
+    std::cout << "Running example_2" << std::endl;
+
+    Chronos::Timers timers;
+    timers.add_timer("timer1");
+    timers.add_timer("timer2");
+
+    timers.get_timer("timer1").start();
+    timers.get_timer("timer2").start();
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Loop 1 iteration " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    timers.get_timer("timer1").pause();
+
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Loop 2 iteration " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    timers.get_timer("timer1").resume();
+
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Loop 1 iteration " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
+    }
+
+    timers.stop_timers();
+    timers.print_timers_elapsed_time();
+
+    return 0;
+}
