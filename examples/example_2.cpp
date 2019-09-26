c5731727683dca9db9bd587df794919572094c6b
Lefteris <el.avramidis@gmail.com>
2019-09-26 20:54:29 +0100

    The artificial computation is done using thread sleep in example 2

--- examples/example_2.cpp
+++ examples/example_2.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include "Timers.hpp"
+#include <thread>
 
 int main() {
     std::cout << "Running example_2" << std::endl;
@@ -18,14 +19,16 @@ int main() {
     timers.get_timer("timer1").start();
     timers.get_timer("timer2").start();
 
-    for (int i = 0; i < 100000; i++) {
-        std::cout << "Hello from example_1\n";
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Loop 1 iteration " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     timers.get_timer("timer2").stop_timer(true);
 
-    for (int i = 0; i < 10000; i++) {
-        std::cout << "Hello from example_1\n";
+    for (int i = 0; i < 10; i++) {
+        std::cout << "Loop 2 iteration " << i << std::endl;
+        std::this_thread::sleep_for(std::chrono::seconds(1));
     }
 
     timers.get_timer("timer1").stop_timer(true);
